bb09888fa69746c830f1a7ffd470780eb987f437
lion88 <lion_alex1988@mail.ru>
2025-04-29 18:00:00 +0300

    KS-8-2 reworked version

--- src/main/kotlin/lesson_8/lesson_8_2.kt
+++ src/main/kotlin/lesson_8/lesson_8_2.kt
@@ -1,25 +1,17 @@
 package lesson_8
 
-//'бекон',  'желтки', "пармезан", "чеснок", "петрушка", "соль"
 fun main() {
     println(
         "Рецепт приготовления пасты карбонары со сливками и беконом"
     )
     println("Какой ингредиент Вы хотите в рецепте найти?")
-    val viewsByDay = arrayOf("пармезан", "чеснок", "петрушка", "соль")
+    val recipe = arrayOf("пармезан", "чеснок", "петрушка", "соль")
     val request = readln()
-    for (i in viewsByDay) {
+    for (i in recipe) {
         if (i == request) {
             println("Ингредиент $request в рецепте есть")
-        } else {
-            println("Ингредиента в рецепте нет")
+            return
         }
     }
+    println("Ингредиента в рецепте нет")
 }
-
-
-
-
-
-
-
