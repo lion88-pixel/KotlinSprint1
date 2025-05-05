7ac79014db854c7f607c57ebeaaf9ddb0cda3cf6
lion88 <lion_alex1988@mail.ru>
2025-05-05 22:58:28 +0300

    KS-9-3 fixed

--- src/main/kotlin/lesson_9/lesson_9_3.kt
+++ src/main/kotlin/lesson_9/lesson_9_3.kt
@@ -4,8 +4,9 @@ fun main() {
     val omeletteIngredients = listOf(2, 50, 15)
     println("Сколько порций омлета Вы желаете?")
     val request = readln().toInt()
-    val numberOfEggs = omeletteIngredients[0] * request
-    val milkVolume = omeletteIngredients[1] * request
-    val quantityOfOil = omeletteIngredients[2] * request
-    println("На $request порции Вам понадобиться: яиц - $numberOfEggs шт., молока - $milkVolume мл, сливочного масла - $quantityOfOil гр.")
+
+    val quantityOfIngredients = omeletteIngredients.map {
+        it * request
+    }
+    println("На $request порции Вам понадобиться: яиц - ${quantityOfIngredients[0]} шт., молока - ${quantityOfIngredients[1]} мл, сливочного масла - ${quantityOfIngredients[2]} гр.")
 }
