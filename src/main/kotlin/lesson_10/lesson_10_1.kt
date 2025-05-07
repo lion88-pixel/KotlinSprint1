a5bae12c66d0e44e8bc182985707951d601aa503
lion88 <lion_alex1988@mail.ru>
2025-05-07 20:50:56 +0300

    KS-10-1 corrected version

--- src/main/kotlin/lesson_10/lesson_10_1.kt
+++ src/main/kotlin/lesson_10/lesson_10_1.kt
@@ -1,17 +1,15 @@
 package lesson_10
 
-import kotlin.random.Random
-
 fun rollDice(): Int {
-    return Random.nextInt(1, 7)
+    return (1..7).random()
 }
+
 fun main() {
     println("Игра: Бросаем кости!")
     val playerThrow = rollDice()
     println("Ход пользователя. Значение броска $playerThrow")
     val computerThrow = rollDice()
     println("Ход компьютера. Значение броска $computerThrow")
-
     if (playerThrow == computerThrow) {
         println("Победила дружба")
     } else if (playerThrow > computerThrow) {
