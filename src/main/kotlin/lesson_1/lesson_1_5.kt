54463d63a34e04d9adbd7e22d28e76a8ae8b6841
lion88 <lion_alex1988@mail.ru>
2025-05-13 11:40:23 +0300

    KS-1-5 corrected version

--- src/main/kotlin/lesson_1/lesson_1_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_5.kt
@@ -1,10 +1,12 @@
 package lesson_1
 
+    const val translationPerSecond = 60
+
 fun main() {
     val totalSecond = 6480
-    val hours = totalSecond / 3600
-    val minutes = (totalSecond % 3600) / 60
-    val seconds = totalSecond % 60
+    val hours = totalSecond / (translationPerSecond * translationPerSecond)
+    val minutes = (totalSecond % (translationPerSecond * translationPerSecond)) / translationPerSecond
+    val seconds = totalSecond % translationPerSecond
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
     println("Время, проведенное в космосе: $formattedTime")
 }
