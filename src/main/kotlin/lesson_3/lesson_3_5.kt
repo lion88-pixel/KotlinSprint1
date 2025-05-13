8a4cbe5308e99b4de5e264275105e058be606e2d
lion88 <lion_alex1988@mail.ru>
2025-05-13 11:50:15 +0300

    KS-3-5 corrected version

--- src/main/kotlin/lesson_3/lesson_3_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_5.kt
@@ -2,8 +2,7 @@ package lesson_3
 
 fun main() {
     val moveData = "D2-D4;0"
-    val (move, moveNumber) = moveData.split(";")
-    val (fromPosition, toPosition) = move.split("-")
+    val (fromPosition, toPosition, moveNumber) = moveData.split("-", ";")
     println("откуда: $fromPosition")
     println("куда: $toPosition")
     println("номер хода: $moveNumber")
