bec93da59876558ba211dc7d2b9061402feb6bb3
lion88 <lion_alex1988@mail.ru>
2025-05-13 22:25:15 +0300

    KS-4-5 corrected version

--- src/main/kotlin/lesson_4/lesson_4_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_5.kt
@@ -1,18 +1,22 @@
 package lesson_4
 
+const val MINIMUM_CREW = 55
+const val MAX_CREW = 70
+const val PROVISION_NORM = 50
+
 fun main() {
     println("наличие повреждеий корпуса (true/false): ")
-    val caseDamage = readln().toBoolean()
+    val isCaseDamage = readln().toBoolean()
     println("текущий состав экипажа: ")
     val crewComposition = readln().toInt()
     println("количество ящиков с провизией на борту: ")
     val boxesOfProvisions = readln().toInt()
     println("благоприятность метеоусловий (true/false): ")
-    val weatherConditions = readln().toBoolean()
-    val canSail =
-        (!caseDamage && crewComposition in 55..70 && boxesOfProvisions > 50 && (weatherConditions || !weatherConditions)) || (crewComposition == 70 && weatherConditions && boxesOfProvisions >= 50)
-
-    if (canSail) {
+    val isWeatherConditions = readln().toBoolean()
+    val isCanSail =
+        (!isCaseDamage && crewComposition in MINIMUM_CREW..MAX_CREW && boxesOfProvisions > PROVISION_NORM ||
+                (crewComposition == MAX_CREW && isWeatherConditions && boxesOfProvisions >= PROVISION_NORM))
+    if (isCanSail) {
         println("корабль может отправляться в плавание")
     } else {
         println("корабль не может отправляться в плавание")
