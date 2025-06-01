b4dd6d3787d4f88ffef21cf42eb01fbacbcbeb49
lion88 <lion_alex1988@mail.ru>
2025-06-01 09:19:12 +0300

    KS-11-1 physically removing

--- src/main/kotlin/lesson_11/lesson_11_1.kt
+++ src/main/kotlin/lesson_11/lesson_11_1.kt
@@ -10,6 +10,6 @@ class User(
 fun main() {
     val userOne = User(1, "user_one", "password1", "user1@mail.ru")
     val userTwo = User(2, "user_two", "password2", "user2@mail.ru")
-    println("user_one: ${userOne.id}, ${userOne.login}, ${userOne.password}, ${userOne.email}")
+    println("user_оne: ${userOne.id}, ${userOne.login}, ${userOne.password}, ${userOne.email}")
     println("user_two: ${userTwo.id}, ${userTwo.login}, ${userTwo.password}, ${userTwo.email}")
 }
