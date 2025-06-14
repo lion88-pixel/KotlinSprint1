f2052533aa29b8f5b7122642c8f74d0e1bed77e5
lion88 <lion_alex1988@mail.ru>
2025-06-14 23:05:10 +0300

    KS-13-1 third option

--- src/main/kotlin/lesson_13/lesson_13_1.kt
+++ src/main/kotlin/lesson_13/lesson_13_1.kt
@@ -6,9 +6,9 @@ class Contact(
     val company: String?
 )
 
-fun main(){
+fun main() {
     val contact1 = Contact("Александр Александров", 89263170915, "МК-М")
     val contact2 = Contact("Петр Петров", 89256676715, null)
-    println(contact1.name+" "+contact1.phoneNumber+" "+contact1.company)
-    println(contact2.name+" "+contact2.phoneNumber+" "+contact2.company)
+    println(contact1.name + " " + contact1.phoneNumber + " " + contact1.company)
+    println(contact2.name + " " + contact2.phoneNumber + " " + contact2.company)
 }
