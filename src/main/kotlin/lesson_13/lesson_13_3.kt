7d10d5c5d55ef16462d1c0c03906a97b56ec31d9
lion88 <lion_alex1988@mail.ru>
2025-06-17 00:47:26 +0300

    KS-13-3 second option

--- src/main/kotlin/lesson_13/lesson_13_3.kt
+++ src/main/kotlin/lesson_13/lesson_13_3.kt
@@ -9,13 +9,15 @@ data class ContactInfo(
 fun main() {
 
     val contacts = listOf(
-        ContactInfo("Александр", 89263170915),
+        ContactInfo("Александр", 89263170915, null),
         ContactInfo("Петр", 89256676715, "MK-M"),
-        ContactInfo("Василий", 89285476715),
+        ContactInfo("Василий", 89285476715, null),
         ContactInfo("Сергей", 89256658725, "null"),
         ContactInfo("Игорь", 89256676578, "ЗаряСталь")
     )
-    contacts.forEach { contact ->
-        println("компания:${contact.company}")
+    val companies = contacts.mapNotNull { it.company }.toSet()
+    println("Список компаний в телефонной книге:")
+    companies.forEach {
+        println(it)
     }
 }
