4045d82b8fc88a307165307f98f8d60f3a11f809
lion88 <lion_alex1988@mail.ru>
2025-07-02 00:11:05 +0300

    KS-16-3 second option

---
+++ src/main/kotlin/lesson_16/lesson_16_3.kt
@@ -0,0 +1,16 @@
+package lesson_16
+
+class User(val login: String, private val passwordHash: Int) {
+
+    fun validatePassword(password: String): Boolean {
+        return password.hashCode() == passwordHash
+    }
+}
+
+fun main() {
+    val user = User("MyLogin", "MySecretPassword".hashCode())
+    print("Введите пароль:")
+    val enteredPassword = readlnOrNull() ?: ""
+    val isPasswordValid = user.validatePassword(enteredPassword)
+    println("Введенный пароль ${if (isPasswordValid) "верен" else "неверен"}.")
+}
