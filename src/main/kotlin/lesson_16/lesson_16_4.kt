0b965494502ad35c80eda541ded89715033e01ae
lion88 <lion_alex1988@mail.ru>
2025-07-02 00:23:39 +0300

    KS-16-4 second option

--- src/main/kotlin/lesson_16/lesson_16_4.kt
+++ src/main/kotlin/lesson_16/lesson_16_4.kt
@@ -2,17 +2,13 @@ package lesson_16
 
 class Order(val orderNumber: Int, private var status: String = "Принят") {
 
-    internal fun changeStatus(newStatus: String) {
+    fun requestStatusChange(newStatus: String) {
+        changeStatus(newStatus)
+    }
+
+    private fun changeStatus(newStatus: String) {
         status = newStatus
         println("Статус заказа №$orderNumber изменен на: $status")
-    }
-
-    fun requestStatusChange(newStatus: String, manager: Manager) {
-        manager.changeOrderStatus(this, newStatus)
-    }
-
-    fun getStatus(): String {
-        return status
     }
 
     fun displayOrderInfo() {
@@ -20,17 +16,10 @@ class Order(val orderNumber: Int, private var status: String = "Принят") {
     }
 }
 
-class Manager {
-    fun changeOrderStatus(order: Order, newStatus: String) {
-        order.changeStatus(newStatus)
-    }
-}
-
 fun main() {
-    val manager = Manager()
     val order = Order(12345)
     order.displayOrderInfo()
-    order.requestStatusChange("В обработке", manager)
-    order.requestStatusChange("Отправлен", manager)
+    order.requestStatusChange("В обработке")
+    order.requestStatusChange("Отправлен")
     order.displayOrderInfo()
 }
