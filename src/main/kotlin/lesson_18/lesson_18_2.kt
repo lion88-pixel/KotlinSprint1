fe3f433c0465f07e85fe0cf52154a6f9968c7f6d
lion88 <lion_alex1988@mail.ru>
2025-07-14 23:51:45 +0300

    KS-18-2 first option

---
+++ src/main/kotlin/lesson_18/lesson_18_2.kt
@@ -0,0 +1,24 @@
+package lesson_18
+
+import kotlin.random.Random
+
+open class Dice(val sides: Int) {
+    open fun roll() {
+        val result = Random.nextInt(1, sides + 1)
+        println("Выпало: $result (Кость с $sides гранями)")
+    }
+}
+
+class Dice4 : Dice(4)
+class Dice6 : Dice(6)
+class Dice8 : Dice(8)
+
+fun main() {
+    val dice4 = Dice4()
+    val dice6 = Dice6()
+    val dice8 = Dice8()
+    val diceList: List<Dice> = listOf(dice4, dice6, dice8)
+    for (dice in diceList) {
+        dice.roll()
+    }
+}
